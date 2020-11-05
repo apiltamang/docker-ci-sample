a135f51034fb5c9415d59c462f240e4c222c356a
Apil Tamang <apil.tamang@gmail.com>
2020-11-05 17:11:44 -0500

    added dockerfile

---
+++ src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,14 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	for (int i=0; i<10; i++)
+        	System.out.println( "Hello World!"+i );
+    }
+}
