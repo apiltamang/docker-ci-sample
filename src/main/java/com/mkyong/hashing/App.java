a01c2ce1ff7dc235046b0bcb994823bf178f62de
Apil Tamang <apil.tamang@gmail.com>
2020-11-05 17:28:56 -0500

    small change

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
     	for (int i=0; i<10; i++)
-        	System.out.println( "Hello World!"+i );
+        	System.out.println( "Hello Apil"+i );
     }
 }
